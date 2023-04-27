93056dd215ff7aae445df6eb3c956935f570464f
Viswa Teja <115293958+v-teja@users.noreply.github.com>
2023-04-27 19:24:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+
+
 function getFormvalue() {
     //Write your code here
-
+	const firstName = document.getElementsByName("fname")[0].value;
+	const lastName = document.getElementsByName("lname")[0].value;
+	alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
+
+
